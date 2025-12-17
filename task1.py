93b9a01a520a09a973eccf880da469d0eabcb5ae
Karen Handy <karen.handy@cmcss.net>
2025-12-16 20:47:04 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,26 +2,26 @@
 
 
 slices = party_pizza_mini + large + medium
-print(f”Number of slices: {slices}”)
+print(f"Number of slices: {slices}")
 
 people += 1
 share = slices//people
 leftover = slices%people
-print(f”Each person gets: {share}”)
-print(f”Slices left: {leftover}”)
+print(f"Each person gets: {share}")
+print(f"Slices left: {leftover}")
 
 people += 2 #Eric and Brandon are coming too.
 share = slices//people
 leftover = slices%people
-print(f”Each person gets: {share}”)
-print(f”Slices left: {leftover}”)
+print(f"Each person gets: {share}")
+print(f"Slices left: {leftover}")
 
-#Mom says “Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
+#Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
 
 slices += party_pizza_mini  		#include tips. people won’t change maybe a hint about adding a mini.
 share = slices//people
 leftover = slices%people
-print(f”Each person gets: {share}”)
-print(f”Slices left: {leftover}”)
+print(f"Each person gets: {share}")
+print(f"Slices left: {leftover}")
 print("...for Mr. Hollow Leg")
 
