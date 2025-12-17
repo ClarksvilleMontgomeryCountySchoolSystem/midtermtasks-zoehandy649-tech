9d6dbd5c2e9cd4d61dc831d6be4ff84a7249ab2c
Karen Handy <karen.handy@cmcss.net>
2025-12-17 09:51:37 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,15 +1,1 @@
-# Week 1: Chores and first purchase
-allowance += dishes
-allowance += lawn
-allowance -= candy
 
-# Week 2: Bonus week and big purchase
-allowance *= 3
-allowance += vacuum
-allowance -= toy
-
-# Week 3: Savings
-allowance /= 2
-
-# Print final allowance
-print(f"Allowance: ${allowance}")
